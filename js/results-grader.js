eee6cbca8cd9676ea1cab4801d9e03c7bfc530ef
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-04-18 14:07:36 +0100

    Add files via upload

--- js/results-grader.js
+++ js/results-grader.js
@@ -3,22 +3,51 @@ function gradeResult(){
 	var myEnglishResult = parseInt(document.getElementById("value1").value);
 	var myIrishResult = parseInt(document.getElementById("value2").value);
 	var myMathsResult = parseInt(document.getElementById("value3").value);
+	
 	var englishGrade;
+	var irishGrade;
+	var mathsGrade;
 	
 	if(!myEnglishResult){
 		alert("Please enter a reuslt!");
 	}
 	else {
 		if(myEnglishResult > 90){
-			grade = 'A';
+			englishGrade = 'A';
 		} else if(myEnglishResult > 80){
-			grade = 'B';
+			englishGrade = 'B';
 		} else {
-			grade = 'F';
+			englishGrade = 'F';
+		}
+	}	
+		
+	if(!myIrishResult){
+		alert("Please enter a result!");
+	}
+	else {
+		if(myIrishResult > 90){
+			irishGrade = 'A';
+		} else if(myIrishResult > 80){
+			irishGrade = 'B';
+		} else {
+			irishGrade = 'F';
+		}
+	}	
+		
+	if(!myMathsResult){
+		alert("Please enter a result!");
+	}
+	else {
+		if(myMathsResult >90){
+			mathsGrade = 'A';
+		} else if(myMathsResult > 80){
+			mathsGrade = 'B';
+		} else {
+			mathsGrade = 'F';
 		}
 		
-		var message = 'Your result of ' + myEnglsihResult + ' gets you a grade ' + englishGrade;
+		//Only output to the HTML File if a result for that subject is entered
 		console.log(message);
-		document.getElementById("resultsentence").innterHTML = message;
+		document.getElementById("resultsentence").innerHTML = message;
 	}
 }
